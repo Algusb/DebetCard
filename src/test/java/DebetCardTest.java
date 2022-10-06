6b2a71e4b184e219ad297fcc0209ee5a3e42f7fb
Aleks Gusev <algusb@mail.ru>
2022-10-07 00:40:16 +0300

    add negativ test

--- src/test/java/DebetCardTest.java
+++ src/test/java/DebetCardTest.java
@@ -23,11 +23,11 @@ public class DebetCardTest {
     @AfterEach
     void teardown() {
         driver.quit();
-//        driver = null;
+        driver = null;
     }
 
     @Test
-    void shouldTestV1() {
+    void shouldTestV1() throws InterruptedException {
         driver.get("http://localhost:7777/");
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иван Петров");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79998887766");
@@ -36,6 +36,21 @@ public class DebetCardTest {
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
         Assertions.assertEquals(expected, actual);
+        Thread.sleep(5000);
+
+    }
+
+    @Test
+    void shouldNotHaveDigitInName() throws InterruptedException {
+        driver.get("http://localhost:7777/");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("12");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79998887766");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button__text")).click();
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        String actual = driver.findElement(By.className("input__sub")).getText().trim();
+        Assertions.assertEquals(expected, actual);
+        Thread.sleep(5000);
 
     }
 
