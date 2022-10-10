6fd1b4573d49ec57d909f46a732d91b79365785c
Aleks Gusev <algusb@mail.ru>
2022-10-10 12:19:24 +0300

    test shouldAcceptYo failed

--- src/test/java/DebetCardTest.java
+++ src/test/java/DebetCardTest.java
@@ -39,18 +39,18 @@ public class DebetCardTest {
 
     }
 
-//    @Test
-//    void shouldAcceptYo() {
-//        driver.get("http://localhost:7777/");
-//        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Фёдор Петров");
-//        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79998887766");
-//        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-//        driver.findElement(By.className("button__text")).click();
-//        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-//        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
-//        Assertions.assertEquals(expected, actual);
-//
-//    }
+    @Test
+    void shouldAcceptYo() {
+        driver.get("http://localhost:7777/");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Фёдор Петров");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79998887766");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button__text")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        Assertions.assertEquals(expected, actual);
+
+    }
 
     @Test
     void shouldAcceptDash() {
